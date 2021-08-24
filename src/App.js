ad77f8221ae01bc355eab25b3284e8452311c1a4
shinhyeonjae <bbbb817@naver.com>
2021-08-24 22:32:51 +0900

    Apply Matrial_ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 //import logo from './logo.svg';
 import './App.css';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+import Customer_ui from './components/Customer_ui';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import { withStyles } from '@material-ui/core/styles';
+import { Component } from 'react';
+
+const styles = theme => ({
+  root:{
+    width : '100%',
+    marginTop : theme.spacing.unit * 3,
+    overflowX : "auto"
+  },
+  table : {
+    minWidth : 1080
+  }
+
+})
 
 const customers = [
   {
     'id' : 1,
-    'image' : 'https://placeimg.com/64/64/1',
+    'image' : 'https://placeimg.com/50/50/1',
     'name' : '홍길동',
     'birthday' : '930817',
     'gender' : '남자',
@@ -13,7 +34,7 @@ const customers = [
   },
   {
     'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
+    'image' : 'https://placeimg.com/50/50/2',
     'name' : '길동',
     'birthday' : '930817',
     'gender' : '남자',
@@ -21,31 +42,36 @@ const customers = [
   },
   {
     'id' : 3,
-    'image' : 'https://placeimg.com/64/64/any',
+    'image' : 'https://placeimg.com/50/50/any',
     'name' : '홍길',
     'birthday' : '93081',
     'gender' : '남',
     'job' : '직장인'
   }
 ]
-function App() {
-  return (
-    <div className="gray-background">
-      {customers.map(c => {
-          return(
-            <Customer
-              key = {c.id}
-              image = {c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job = {c.job}
-            />
-          )
-      })}
-        
-    </div>
-  );
+class App extends Component{
+  render(){
+    const {classes} = this.props; //props 한번에 변수로 선언
+    return (
+      <Paper className = {classes.root}>
+        <Table className = {classes.table}>
+          <TableHead>
+            <TableRow>
+                  <TableCell>번호</TableCell>
+                  <TableCell>이미지</TableCell>
+                  <TableCell>이름</TableCell>
+                  <TableCell>생년월일</TableCell>
+                  <TableCell>성별</TableCell>
+                  <TableCell>직업</TableCell>
+              </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {return(<Customer_ui key = {c.id} id = {c.id} image = {c.image} name={c.name} birthday={c.birthday} gender={c.gender} job = {c.job} />) })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
